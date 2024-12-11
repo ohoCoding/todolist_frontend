f71a89e85be409e3d472d97ba7697ef4ee96e2c9
ohoCoding <youngho7482@gmail.com>
2024-12-11 13:39:25 +0900

    feat: :sparkles: setting color themes

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          900: "#0F172A",
+          800: "#1E293B",
+          500: "#64748B",
+          400: "#94A3B8",
+          300: "#CBD5E1",
+          200: "#E2E8F0",
+          100: "#F1F5F9",
+        },
+        violet: {
+          600: "#7C3AED",
+          100: "#EDE9FE",
+        },
+        rose: {
+          500: "#F43F5E",
+        },
+        lime: {
+          300: "#BEF264",
+        },
+        amber: {
+          800: "#92400E",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
